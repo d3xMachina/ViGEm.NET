06cd489b0969879b5c2af85df4dddc817ec67c75
d3xMachina <16732772+d3xMachina@users.noreply.github.com>
2024-06-08 23:47:42 +0200

    fix wrong winapi error message showing

--- ViGEmClient/ViGEmClient.Native.cs
+++ ViGEmClient/ViGEmClient.Native.cs
@@ -22,128 +22,128 @@ using PVIGEM_USER_DATA = IntPtr;
 [SuppressMessage("ReSharper", "UnusedMember.Global")]
 public sealed partial class ViGEmClient
 {
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     private static extern PVIGEM_CLIENT vigem_alloc();
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     private static extern void vigem_free(
         PVIGEM_CLIENT vigem);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     private static extern VIGEM_ERROR vigem_connect(
         PVIGEM_CLIENT vigem);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     private static extern void vigem_disconnect(
         PVIGEM_CLIENT vigem);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     internal static extern PVIGEM_TARGET vigem_target_x360_alloc();
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     internal static extern PVIGEM_TARGET vigem_target_ds4_alloc();
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     internal static extern void vigem_target_free(
         PVIGEM_TARGET target);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     internal static extern VIGEM_ERROR vigem_target_add(
         PVIGEM_CLIENT vigem,
         PVIGEM_TARGET target);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     private static extern VIGEM_ERROR vigem_target_add_async(
         PVIGEM_CLIENT vigem,
         PVIGEM_TARGET target,
         PVIGEM_TARGET_ADD_RESULT result);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     internal static extern VIGEM_ERROR vigem_target_remove(
         PVIGEM_CLIENT vigem,
         PVIGEM_TARGET target);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     internal static extern VIGEM_ERROR vigem_target_x360_register_notification(
         PVIGEM_CLIENT vigem,
         PVIGEM_TARGET target,
         PVIGEM_X360_NOTIFICATION notification);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     internal static extern VIGEM_ERROR vigem_target_ds4_register_notification(
         PVIGEM_CLIENT vigem,
         PVIGEM_TARGET target,
         PVIGEM_DS4_NOTIFICATION notification);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     internal static extern void vigem_target_x360_unregister_notification(
         PVIGEM_TARGET target);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     internal static extern void vigem_target_ds4_unregister_notification(
         PVIGEM_TARGET target);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     internal static extern void vigem_target_set_vid(
         PVIGEM_TARGET target,
         ushort vid);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     internal static extern void vigem_target_set_pid(
         PVIGEM_TARGET target,
         ushort pid);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     private static extern ushort vigem_target_get_vid(
         PVIGEM_TARGET target);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     private static extern ushort vigem_target_get_pid(
         PVIGEM_TARGET target);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     internal static extern VIGEM_ERROR vigem_target_x360_update(
         PVIGEM_CLIENT vigem,
         PVIGEM_TARGET target,
         XUSB_REPORT report);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     internal static extern VIGEM_ERROR vigem_target_ds4_update(
         PVIGEM_CLIENT vigem,
         PVIGEM_TARGET target,
         DS4_REPORT report);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     internal static extern VIGEM_ERROR vigem_target_ds4_update_ex(
         PVIGEM_CLIENT vigem,
         PVIGEM_TARGET target,
         DS4_REPORT_EX report);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     private static extern uint vigem_target_get_index(
         PVIGEM_TARGET target);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     private static extern VIGEM_TARGET_TYPE vigem_target_get_type(
         PVIGEM_TARGET target);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     private static extern bool vigem_target_is_attached(
         PVIGEM_TARGET target);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     private static extern VIGEM_ERROR vigem_target_x360_get_user_index(
         PVIGEM_CLIENT vigem,
         PVIGEM_TARGET target,
         out UInt32 index);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     internal static extern VIGEM_ERROR vigem_target_ds4_await_output_report(
         PVIGEM_CLIENT vigem,
         PVIGEM_TARGET target,
         ref DS4_AWAIT_OUTPUT_BUFFER buffer);
 
-    [DllImport("vigemclient.dll", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("vigemclient.dll", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
     internal static extern VIGEM_ERROR vigem_target_ds4_await_output_report_timeout(
         PVIGEM_CLIENT vigem,
         PVIGEM_TARGET target,
